cf5ed8f93ce5d5e325e40056b8513681335af25c
reishisu <poiponn697@gmail.com>
2018-07-20 05:19:36 +0900

    【削除】いくつかの関数を削除しました。 【追加】画像の書き込みなどを把握する進捗バーを作成しました。

--- nk/opencv.py
+++ nk/opencv.py
@@ -1,6 +1,10 @@
 '''
 OpenCVの使ってみるファイル
-予めpip install opencv-pythonでOpenCVをインストールしておく
+予め
+　pip install opencv-pythonでOpenCV
+　pip install tqdm
+をしておく
+
 
 OpenCVで読み込む画像の構造
 # 画像を1つに対して１つの配列
@@ -18,49 +22,42 @@ OpenCVで読み込む画像の構造
 import cv2
 
 # 各種ライブラリをインポート
-import os
-
-# 画像のフォルダパス
-imagePath = "./images/"
-
-# テキストファイルの出力先のパス
-textExportPath = "./rgbtext/"
+import os, sys
+from tqdm import tqdm
 
 # コンソールをクリアする
 os.system('clear')
 
-# 画像を読み込み(無ければsampel.jpg)
-fileName = input()
-img = cv2.imread(imagePath + fileName)
-if img is None:
-    print("そのファイル名の画像は見つけられませんでした...")
-    fileName = "sample.jpg"
-    img = cv2.imread(imagePath + fileName)
+# 画像を読み込み
+fileName = sys.argv[1]
+img = cv2.imread(fileName)
 
 '''
 メイン関数
 '''
 def main():
-    inFileAllPixel( anyFileNameExetention( textExportPath, fileName, "txt" ) )
-    
+    WriteInFileAllPixel( anyFileNameExetention( fileName, "txt" ) )
+
+
 '''
 テキストファイルにRGBの値を全部保存
 '''
-def inFileAllPixel(name):
+def WriteInFileAllPixel(name):
+    line_num = 1
     # ファイルを開く(無ければ新規作成)
     f = open(name, 'w')
     # 縦の行のループ
-    for height, line_num in zip( img, range( 1, len( img ) ) ):
-        print( str( line_num ) + "行目を書き込み中" )
+    for height in img:
         # 行数をファイルに書き込み
-        f.write( str( line_num ).ljust(3," ") + "| " )
+        f.write( str( line_num ).rjust(3) + "| " )
         # 横の行のループ
         for width in height:
             # 各ピクセルのRGB値を書き込み
-            # f.write( str( width ) + " " )
             f.write( str( width ))
-        # 行の終わりに改行させる
-        f.write("\n")
+        print( "\r進捗率　：　[" + "#" * int(line_num/len(img)*100) + " " * (100 - int(line_num/len(img)*100)) + "] " + str(line_num) + "/" + str(len(img)) + "書き込み済", end="" )
+        line_num += 1
+    print("")
+
 
 '''
 画像のRGB値を全部表示
@@ -70,11 +67,11 @@ def showAllPixel():
     line_num = 1
     # 縦の行のループ
     for height in img:
-        print( str( line_num ).ljust(3," ") + "| ", end="" )
+        print( str( line_num ).rjust(3) + "| ", end="" )
         # 横の行のループ
         for width in height:
             print( str( width ), end="" )
-        count += 1
+        line_num += 1
         print("")
 
 
@@ -82,47 +79,24 @@ def showAllPixel():
 画像の真ん中の列のRGB値を全部表示
 '''
 def showAllCenterPixel():
+    line_num = 1
     print("画像真ん中の列のRGB値")
     for px in img:
         # 真ん中の列のピクセルのRGBを上から下に表示
-        print( px[ int( len(img)/2 ) ] )
+        print( str( line_num ).rjust(3) + "| " + str( px[ int( len(img)/2 ) ] ) )
+        line_num += 1
 
 '''
-最後の.以外-に変換する関数
-'''
-def changeDotToBar( String: str ):
-    # 分割
-    sliceString = String.split(".")
-    newString = ""
-
-    # ドットの数が１つの時
-    if len( sliceString ) == 2:
-        print("正しいフォーマットです！")
-        print("入力文字列： " + String)
-        print("出力文字列： " + String)
-        return String
-
-    # 変換処理
-    print(len( sliceString ))
-    print(sliceString)
-    for s, i in zip( sliceString, range( 0, len( sliceString ) ) ):
-        if i == len( sliceString )-1:
-            newString += s
-            break
-        newString += s + "-"
-        print(newString)
-    print("入力文字列： " + String)
-    print("出力文字列： " + newString)
-    return newString
-
-
-'''
-任意でパスも指定できる
 名前に任意の拡張子をつけて返す関数
 '''
-def anyFileNameExetention(path: str, oldName: str, extention: str):
+def anyFileNameExetention(oldName: str, extention: str):
+    newFileName = ""
+    tmp = oldName.split(".")
+    for i in range( 0, len(tmp)-1 ):
+        newFileName += tmp[i]
     # 最後の.以外を取り除いて新しい拡張子をつけて返す
-    return path + changeDotToBar(oldName).split(".")[0] + "." + extention
+    print("newFileName = " + newFileName + "." + extention)
+    return newFileName + "." + extention
 
 
 '''
