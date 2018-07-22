657744283c947d541b0f70d31d254f821d229497
reishisu <poiponn697@gmail.com>
2018-07-23 07:30:25 +0900

    【削除】演算処理だけの場合はGILというものでマルチスレッドの場合余計に遅くなってしまうのでスレッドの部分は削除しました。

--- nk/ParallelMono.py
+++ nk/ParallelMono.py
@@ -33,38 +33,24 @@ useThread = 1
 useCPU = 1
 
 def main():
-    # スレッドかCPUか選ぶ
-    msg = "マルチスレッドかマルチプロセスどちらにしますか？\n"\
-          "[1:マルチスレッド 2:マルチプロセス] : "
-    multchType = int( input(msg.format(os.cpu_count())) )
-    # 使用する数を選択
-    if multchType == 1:
-        useThread = int( input("使用するスレッドの数を入力してください : ") )
-        if useThread > 1:
-            mulchThread(useThread= useThread)
-            plt.imshow(img)
-            plt.show()
-        else:
-            print("0以下なので終了")
-    elif multchType == 2:
-        useCPU = int( input("使用するCPUのコアを入力してください[ 1 ~ {0} ] : ".format(os.cpu_count())) )
-        if useCPU >= 1 and useCPU <= os.cpu_count():
-            mulchProcess(useCPU= useCPU)
-            plt.imshow(img)
-            plt.show()
-        else:
-            print("選択の範囲外なので終了")
+    useCPU = int( input("使用するCPUのコアを入力してください[ 1 ~ {0} ] : ".format(os.cpu_count())) )
+    if useCPU >= 1:
+        if useCPU > os.cpu_count():
+            useCPU = os.cpu_count()
+        mulchProcess(useCPU= useCPU)
+        plt.imshow(img)
+        plt.show()
     else:
-        print("どちらでもないので終了")
+        print("選択の範囲外なので終了")
 
 
 
 def changeToGray( number: int, width: np.ndarray ):
     """
     並列化する処理
-    @param  number (int)       : このスレッドの番号
+    @param  number (int)       : このプロセスの番号
     @param  width (np.ndarray) : 横１行の配列[ [R, G, B], ・・・・ ,[R, G, B] ]
-    @return number (int)       : このスレッドの番号
+    @return number (int)       : このプロセスの番号
     @return width (np.ndarray) : 引数で受け取った配列をグレースケールに変換した配列
     """
     for pixel in width:
@@ -88,28 +74,6 @@ def mulchProcess(useCPU: int):
     count = 0
     print("{0}コアで処理を開始します!!".format(useCPU))
     with concurrent.futures.ProcessPoolExecutor(max_workers=useCPU) as executer:
-        fs = [ executer.submit(changeToGray, i, width) for width, i in zip( img, range(len(img)) ) ]
-        for future in concurrent.futures.as_completed(fs):
-            line_number = future.result()[0]
-            gray_width  = future.result()[1]
-            img[line_number] = gray_width
-            count += 1
-            common.progressBar(count, len(img))
-    print("\n終了しました!!")
-    print("かかった時間:{0}秒".format( time.time()-start ))
-
-
-
-def mulchThread(useThread: int):
-    """
-    スレッドを生成して実行させる処理
-    @param  useThread (int)  : 使用するスレッドの数
-    """
-    print("")
-    start = time.time()
-    count = 0
-    print("{0}スレッドで処理を開始します!!".format(useThread))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=useThread) as executer:
         fs = [ executer.submit(changeToGray, i, width) for width, i in zip( img, range(len(img)) ) ]
         for future in concurrent.futures.as_completed(fs):
             line_number = future.result()[0]
